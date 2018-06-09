0d384a48e23e792a6e8c48b6a46abe822e1ec09f
zpy007 <zpy2358@gmail.com>
2018-06-09 11:31:40 +0800

    使用了MapperFactoryBean 使用xml文件关联mapper接口与sql语句，xml文件默认在classpath位置，就是resources文件夹

--- src/main/java/com/zpy/dao/ClientsMapper.java
+++ src/main/java/com/zpy/dao/ClientsMapper.java
@@ -1,7 +1,11 @@
 package com.zpy.dao;
 
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+
 import java.util.HashMap;
 
 public interface ClientsMapper {
-    HashMap selectClient(String id);
+    //@Select("select * from clients where client_id=#{id}")
+    HashMap selectClient(/*@Param("id")*/ String id);
 }
